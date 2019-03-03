c42b22f5d5dfe84cd6e75e62ceddd241f4ecf1b4
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2019-03-03 02:54:58 -0500

    Add House Info Controller Class

---
+++ iOS/ARKitInteraction/HooHacks/HouseInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HouseInfoViewController.swift
+//  ARKitInteraction
+//
+//  Created by Apollo Zhu on 3/3/19.
+//  Copyright © 2019 <script>alert("Who Hacks?")</script>. All rights reserved.
+//
+
+import UIKit
+
+class HouseInfoViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
